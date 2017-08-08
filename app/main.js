8aa25f680e4d721ada32a75e6578e50912545752
Karan Sharma <sharmak@crossmuller.com.au>
2017-08-08 14:26:29 +1000

    Initial electron app settings

--- app/main.js
+++ app/main.js
@@ -8,7 +8,8 @@ let win;
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 800, height: 600, toolbar: false });
+    win.setMenuBarVisibility(false);
 
     // and load the index.html of the app.
     win.loadURL(url.format({
@@ -16,9 +17,6 @@ function createWindow() {
         protocol: 'file:',
         slashes: true
     }));
-
-    // Open the DevTools.
-    win.webContents.openDevTools()
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -32,7 +30,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
